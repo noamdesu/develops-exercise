10cf68cf9e23dbf1f4416baf4e5944e2b798d268
Noam Sellam <nompops@icloud.com>
2017-06-19 09:04:31 +0300

    Initial Commit

---
+++ devslopes-exercise/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  devslopes-exercise
+//
+//  Created by Noam Sellam on 19/06/2017.
+//  Copyright © 2017 Noam Sellam. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ devslopes-exercise/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  devslopes-exercise
+//
+//  Created by Noam Sellam on 19/06/2017.
+//  Copyright © 2017 Noam Sellam. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
